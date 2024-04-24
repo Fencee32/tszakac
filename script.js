aa298bd30a6ca70a14fb469ace58b98785ee17cc
Fence32 <bence00504@gmail.com>
2024-04-24 13:41:20 +0200

    files generated, changed.

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    header.classList.toggle("scrolled", window.scrollY > 0);
+});
